07b73ca8090053954952ebaf1f97eea2186f0a27
Gergely Brautigam <182850+Skarlso@users.noreply.github.com>
2024-03-28 08:21:08 +0100

    using my cache

---
+++ day12/part1/go.mod
@@ -0,0 +1,5 @@
+module part1
+
+go 1.22.0
+
+require github.com/Skarlso/cache v0.0.0-20240328071535-150ce8ba44fb
--- day12/part1/main.go
+++ day12/part1/main.go
@@ -5,13 +5,9 @@ import (
 	"os"
 	"strconv"
 	"strings"
-)
 
-// args for the function to cache.
-type args struct {
-	conditions string
-	rules      string
-}
+	"github.com/Skarlso/cache"
+)
 
 func main() {
 	if len(os.Args) < 2 {
@@ -22,59 +18,61 @@ func main() {
 
 	content, _ := os.ReadFile(file)
 	split := strings.Split(string(content), "\n")
-	cache := make(map[args]int)
+	c := cache.New[int]()
 	result := 0
 	for _, l := range split {
 		arg := strings.Split(l, " ")
 		conditions, rules := arg[0], arg[1]
 		rulesList := strings.Split(rules, ",")
-		result += calculate(cache, conditions, rulesList)
+		result += c.WithCache(calculate(conditions, rulesList), conditions, rulesList)
 	}
 
 	fmt.Println("result: ", result)
 }
 
-func calculate(cache map[args]int, conditions string, rules []string) int {
-	if v, ok := cache[args{conditions: conditions, rules: strings.Join(rules, ",")}]; ok {
-		return v
-	}
+func calculate(conditions string, rules []string) cache.Cacheable[int] {
+	return func() int {
+		//if v, ok := cache[args{conditions: conditions, rules: strings.Join(rules, ",")}]; ok {
+		//	return v
+		//}
 
-	if len(rules) == 0 {
-		if strings.Contains(conditions, "#") {
-			cache[args{conditions: conditions, rules: strings.Join(rules, "")}] = 0
+		if len(rules) == 0 {
+			if strings.Contains(conditions, "#") {
+				//cache[args{conditions: conditions, rules: strings.Join(rules, "")}] = 0
+				return 0
+			}
+
+			//cache[args{conditions: conditions, rules: strings.Join(rules, "")}] = 1
+			return 1
+		}
+		if len(conditions) == 0 {
+			if len(rules) == 0 {
+				//cache[args{conditions: conditions, rules: strings.Join(rules, "")}] = 1
+				return 1
+			}
+
+			//cache[args{conditions: conditions, rules: strings.Join(rules, "")}] = 0
 			return 0
 		}
 
-		cache[args{conditions: conditions, rules: strings.Join(rules, "")}] = 1
-		return 1
-	}
-	if len(conditions) == 0 {
-		if len(rules) == 0 {
-			cache[args{conditions: conditions, rules: strings.Join(rules, "")}] = 1
-			return 1
+		var result int
+
+		if conditions[0] == '.' || conditions[0] == '?' {
+			result += calculate(conditions[1:], rules)()
 		}
 
-		cache[args{conditions: conditions, rules: strings.Join(rules, "")}] = 0
-		return 0
-	}
-
-	var result int
-
-	if conditions[0] == '.' || conditions[0] == '?' {
-		result += calculate(cache, conditions[1:], rules)
-	}
-
-	if conditions[0] == '#' || conditions[0] == '?' {
-		index, _ := strconv.Atoi(rules[0])
-		if index <= len(conditions) && !strings.Contains(conditions[:index], ".") && (index == len(conditions) || conditions[index] != '#') {
-			if index+1 >= len(conditions) {
-				result += calculate(cache, "", rules[1:])
-			} else {
-				result += calculate(cache, conditions[index+1:], rules[1:])
+		if conditions[0] == '#' || conditions[0] == '?' {
+			index, _ := strconv.Atoi(rules[0])
+			if index <= len(conditions) && !strings.Contains(conditions[:index], ".") && (index == len(conditions) || conditions[index] != '#') {
+				if index+1 >= len(conditions) {
+					result += calculate("", rules[1:])()
+				} else {
+					result += calculate(conditions[index+1:], rules[1:])()
+				}
 			}
 		}
-	}
 
-	cache[args{conditions: conditions, rules: strings.Join(rules, "")}] = result
-	return result
+		//cache[args{conditions: conditions, rules: strings.Join(rules, "")}] = result
+		return result
+	}
 }
